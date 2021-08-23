ed18dee24c8ff069633c07fb0615d66ba44d52a6
Maksim Bulenkov <bulenkovmaksim89@gmail.com>
2021-08-23 17:49:54 +0300

    init

---
+++ src/main/java/org/bmn/telegrampooling/TelegramPoolingApplication.java
@@ -0,0 +1,13 @@
+package org.bmn.telegrampooling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramPoolingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramPoolingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/bmn/telegrampooling/TelegramPoolingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bmn.telegrampooling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramPoolingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
